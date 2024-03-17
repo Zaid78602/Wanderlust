4048da76314111338a006a76be685ff84fa8106b
Mohd Zaid <mohdzaidansari786024@gmail.com>
2024-03-17 15:41:39 +0530

    correct spelling

--- schema.js
+++ schema.js
@@ -3,10 +3,10 @@ const Joi = require("joi");
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
-        discription: Joi.string().required(),
+        description: Joi.string().required(),
         image: Joi.string().allow("", null),
         price: Joi.number().required().min(0),
-        country: Joi.string().required,
+        country: Joi.string().required(),
         location: Joi.string().required(),
     }).required(),
 });
